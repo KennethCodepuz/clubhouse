05cf80c4261c15f869f2bdf021f0c462f80c18b6
KennethCodepuz <kennethseancorpuz14@gmail.com>
2025-12-21 15:58:35 +0800

    remove the individual pages and created 1 page the will render all different group pages in it

--- controllers/groupController.js
+++ controllers/groupController.js
@@ -6,7 +6,9 @@ const groupGetPosts = async (req, res) => {
   const {group} = req.params;
 
   const groups = await getAllGroupNames();
-  const groupNames = groups.map(group => group.name).map(grup => grup.replaceAll(' ', ''));
+  const groupNames = groups.map(group => group.name).map(grup => grup.replace(/\s+/g, '-'));
+
+  console.log(group);
   
   if(!groupNames.includes(group)) {
     return res.render('404');
@@ -15,7 +17,7 @@ const groupGetPosts = async (req, res) => {
   try {
     const data = await getAllPostByGroup(group);
     const formatData = data === undefined ? [] : data;
-    return res.render(`${group}`, { groupName: group, contents: formatData });
+    return res.render(`group-page`, { groupName: group, contents: formatData });
   }catch (err) {
     console.log(err);
   }
