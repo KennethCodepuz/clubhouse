4be5aadc7376ba9df93c917b1678af70125ad003
KennethCodepuz <kennethseancorpuz14@gmail.com>
2025-12-26 22:08:13 +0800

    Prepare for Render deployment

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.get('/', checkUser, requireAuth, async (req, res) => {
   }
 });
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT,  () => {
   console.log(`App is running on port ${PORT}`);
--- db/pool.js
+++ db/pool.js
@@ -2,11 +2,10 @@ const { Pool } = require('pg')
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DBUSER,
-  password: process.env.DBPASSWORD,
-  database: process.env.DBDATABASE,
-  host: process.env.HOST,
-  port: 5432
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 })
 
 module.exports = pool;
